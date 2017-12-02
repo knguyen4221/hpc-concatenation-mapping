072c43a477e404edb3b3ed3c4c514d400379dbb9
knguyen4221 <kenqn@uci.edu>
2017-12-01 16:14:28 -0800

    outputs both fpkms and counts

--- matrixOutput.py
+++ matrixOutput.py
@@ -10,6 +10,7 @@ def main():
 	fastqDir = sys.argv[1]
 	outputFile = sys.argv[2]
 	matrixOutput("counts", fastqDir, outputFile)
+	matrixOutput("fpkm", fastqDir, outputFile)
 
 def matrixOutput(fpkmCount, fastqDir, outputFile):
 	line = 'genes '
@@ -17,6 +18,7 @@ def matrixOutput(fpkmCount, fastqDir, outputFile):
 		with open("{0}/{1}/{1}.{2}".format(fastqDir, directory, fpkmCount), 'r') as infile:
 			data = infile.readlines()
 		name = directory.split("_",1)
+		#replaces top name of file
 		try:
 			data[0] = name[1] + "\n"
 		except IndexError:
@@ -24,7 +26,7 @@ def matrixOutput(fpkmCount, fastqDir, outputFile):
 		with open("{0}/{1}/{1}.{2}".format(fastqDir, directory, fpkmCount), 'w') as outfile:
 			outfile.writelines(data)
 		line += "{0}/{1}/{1}.{2} ".format(fastqDir, directory, fpkmCount)
-	with open("fullMatrix/{0}.counts".format(outputFile), 'w') as outfile:
+	with open("fullMatrix/{0}.{1}".format(outputFile, fpkmCount), 'w') as outfile:
 		subprocess.call(['paste', '--delimiters=,'] + line.split(), stdout=outfile)
 				
 
